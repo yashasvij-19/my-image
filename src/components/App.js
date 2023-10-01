3b98ac1555b4772b0040cbce8b5f408c05ff03dc
Yashasvi <yashasvivij01@gmail.com>
2023-10-01 21:42:22 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,56 @@
-import React, { useEffect, useState } from 'react'
-import '../styles/App.css';
-import {Loader} from './Loader';
-import {PhotoFrame}  from './PhotoFrame';
+import React, { useEffect, useState } from "react";
+import "../styles/App.css";
+import { Loader } from "./Loader";
+import { PhotoFrame } from "./PhotoFrame";
+import axios from "axios";
+
 const App = () => {
+  const [id, setId] = useState(1);
+  const [url, setUrl] = useState("");
+  const [title, setTitle] = useState("");
+  const [loader, setLoader] = useState(true);
+  const [showData, setshowData] = useState(false);
 
-    const [id,setId] = useState(1);
-    const [url,setUrl] = useState("");
-    const [title,setTitle] = useState(""); 
-    const [loader,setLoader] = useState(true);
-    const [showData,setshowData] = useState(false);
-
-    useEffect(()=>{
-       fetch("https://jsonplaceholder.typicode.com/photos/"+id)
-       .then((res) => res.json())
-       .then((data) => {
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/photos/" + id)
+      .then((response) => {
+        const data = response.data;
         setUrl(data.url);
         setTitle(data.title);
         setLoader(false);
-       });
-    },[id])
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setLoader(false);
+      });
+  }, [id]);
 
-    return(
-        <div>
-            <input type="number" min="1" max="5000" onChange={(e) => {
-                setId(e.target.value);
-                setshowData(true);
-                setLoader(true)
-                }}/>
-            {showData ? (loader ? <Loader /> : url && title && (loader ? <Loader /> : <PhotoFrame url={url} title={title} />)) : ""}
-        </div>
-    )
-}
-
+  return (
+    <div>
+      <input
+        type="number"
+        min="1"
+        max="5000"
+        onChange={(e) => {
+          setId(e.target.value);
+          setshowData(true);
+          setLoader(true);
+        }}
+      />
+      {showData ? (
+        loader ? (
+          <Loader />
+        ) : (
+          url &&
+          title &&
+          (loader ? <Loader /> : <PhotoFrame url={url} title={title} />)
+        )
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
 
 export default App;
