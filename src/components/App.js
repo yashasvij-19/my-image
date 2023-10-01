d96e85583d8f029e63dfcf1bc39c02f94aa75886
Yashasvi <yashasvivij01@gmail.com>
2023-10-01 21:49:10 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import "../styles/App.css";
 import { Loader } from "./Loader";
 import { PhotoFrame } from "./PhotoFrame";
